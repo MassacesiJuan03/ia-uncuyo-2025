ff00651992bce75a054b193ded5d383614813709
Juan Ignacio Massacesi <111523496+MassacesiJuan03@users.noreply.github.com>
2025-08-25 14:44:26 -0700

    Update main.py

--- tp3-algoritmos-busqueda/code/main.py
+++ tp3-algoritmos-busqueda/code/main.py
@@ -1,6 +1,10 @@
 import random
 import time
 import pandas as pd
+import gymnasium as gym
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+from gymnasium import wrappers
+
 from map import generate_random_map_custom
 from search_functions import bfs_search, dfs_search, uniform_cost_search, a_star_search
 import concurrent.futures
@@ -16,80 +20,26 @@ def actions_cost_esc2(path):
             cost += 1
     return cost
 
-def run_env(env_n):
-    mapa, start, goal = generate_random_map_custom(20, 0.92)
-
-    #imprimir mapa
-    # print(f"Environment {env_n}:")
-    # for row in mapa:
-    #     print(" ".join(row))
-    # print(f"Start: {start}, Goal: {goal}\n")
-    # print("-" * 40)
+def generate_envs():
+    maps = []
+    for i in range(30):
+        maps.append(generate_random_map_custom(20,0.92))
     
-    # BFS
+    return maps
+    
+
+def run_environment(env_n, func, map):
+    mapa, start, goal = map
+    
     t0 = time.time()
-    found, path = bfs_search(mapa, start)
+    found, path = func(mapa, start, goal)
     t1 = time.time()
     states_n = len(path) 
     actions_count = len(path) - 1 if found else 0
     actions_cost = actions_cost_esc2(path) if found else 0
      
-    result_bfs = {
-        "algorithm_name": "BFS",
-        "env_n": env_n,
-        "states_n": states_n,
-        "actions_count": actions_count,
-        "actions_cost": actions_cost,
-        "time": t1 - t0,
-        "solution_found": found
-    }
-
-    # DFS
-    t0 = time.time()
-    found, path = dfs_search(mapa, start)
-    t1 = time.time()
-    states_n = len(path) 
-    actions_count = len(path) - 1 if found else 0
-    actions_cost = actions_cost_esc2(path) if found else 0
-            
-    result_dfs = {
-        "algorithm_name": "DFS",
-        "env_n": env_n,
-        "states_n": states_n,
-        "actions_count": actions_count,
-        "actions_cost": actions_cost,
-        "time": t1 - t0,
-        "solution_found": found
-    }
-
-    # UCS
-    t0 = time.time()
-    found, path = uniform_cost_search(mapa, start)
-    t1 = time.time()
-    states_n = len(path) 
-    actions_count = len(path) - 1 if found else 0
-    actions_cost = actions_cost_esc2(path) if found else 0
-    
-    result_ucs = {
-        "algorithm_name": "UCS",
-        "env_n": env_n,
-        "states_n": states_n,
-        "actions_count": actions_count,
-        "actions_cost": actions_cost,
-        "time": t1 - t0,
-        "solution_found": found
-    }
-
-    # A*
-    t0 = time.time()
-    found, path = a_star_search(mapa, start, goal)
-    t1 = time.time()
-    states_n = len(path) 
-    actions_count = len(path) - 1 if found else 0
-    actions_cost = actions_cost_esc2(path) if found else 0
-    
-    result_aStar = {
-        "algorithm_name": "A*",
+    results = {
+        "algorithm_name": func.__name__,
         "env_n": env_n,
         "states_n": states_n,
         "actions_count": actions_count,
@@ -98,17 +48,50 @@ def run_env(env_n):
         "solution_found": found
     }
     
-    return [result_bfs, result_dfs, result_ucs, result_aStar]
-
+    return results
+    
 def main():
     all_results = []
-    for i in range(30):
-        all_results.extend(run_env(i))
- 
+    maps = generate_envs()
+    search_funcs = [bfs_search, dfs_search, uniform_cost_search, a_star_search]
+    
+    for func in search_funcs:
+        env_n = 1
+        for map in maps:
+            result = run_environment(env_n, func, map)
+            all_results.append(result)
+            env_n += 1
+        
     df = pd.DataFrame(all_results)
     df.to_csv('results.csv', index=False)
     return df
-        
+    
 
 if __name__ == "__main__":
-    main()
+    #main()
+    mapas = generate_envs()
+    desc = mapas[0][0]
+    
+    nuevo_limite = 5
+    env = gym.make('FrozenLake-v1', desc=desc, render_mode='ansi', is_slippery=False)
+    env = wrappers.TimeLimit(env, nuevo_limite)
+    
+    env.reset()
+    grid = env.render()
+    
+    print(grid)
+
+    #found, path = bfs_search(env, mapas[0][1], mapas[0][2])
+
+
+    # done = truncated = False
+    # i=0
+    # while not (done or truncated):
+    #     action = env.action_space.sample()
+    #     i+=1
+    #     # Acci´on aleatoria
+    #     next_state, reward, done, truncated, _ = env.step(action)
+    #     grid = env.render()
+    #     print(grid)
+    #     print(f"Acci´on: {action}, Nuevo estado: {next_state}, Recompensa: {reward}")
+    #     state = next_state
